bef929e11e8d0358de60e646bb64560129080c7b
Nikhil Mehta <nikhil.001mehta@gmail.com>
2023-11-23 22:59:56 +0530

    fix(otp-input): fixing for android backspace keycode issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import React from "react";
-import { useState, useCallback, useEffect, useRef } from "react";
+import React, { ChangeEvent } from "react";
+import { useState, useCallback, useLayoutEffect, useRef } from "react";
 import { MouseEvent } from "react";
 import "./index.css";
 
@@ -19,6 +19,7 @@ interface SingleDigitInputPropsType {
   val: string;
   focus: boolean;
   index: number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function OtpInput({ length = 6 }: OtpInputPropsType) {
@@ -26,7 +27,7 @@ export function OtpInput({ length = 6 }: OtpInputPropsType) {
   const [value, setValue] = useState<string[]>(new Array(6).fill(""));
   const handlePaste = useCallback((event: React.ClipboardEvent) => {
     const data = event.clipboardData.getData("text/plain");
-    // Should we show error if user tries to paste
+    // Should we show error if user tries to paste alphabets
     if (data.length === length && !isNaN(Number(data))) {
       setValue(data.split(""));
       setFocusOn(length - 1);
@@ -34,14 +35,12 @@ export function OtpInput({ length = 6 }: OtpInputPropsType) {
   }, []);
   const handleKeyDown = useCallback(
     (e: React.KeyboardEvent) => {
+      debugger;
       if (e.key === "ArrowLeft") {
         setFocusOn(focusOn - 1);
         setFocusOn(Math.max(focusOn - 1, 0));
       } else if (e.key === "ArrowRight") {
         setFocusOn(Math.min(focusOn + 1, length - 1));
-      } else if (e.key === "Backspace" || e.key === "Delete") {
-        value[focusOn] = "";
-        setFocusOn(Math.max(focusOn - 1, 0));
       } else if (!isNaN(Number(e.key))) {
         value[focusOn] = e.key;
         setFocusOn(Math.min(focusOn + 1, length - 1));
@@ -55,9 +54,18 @@ export function OtpInput({ length = 6 }: OtpInputPropsType) {
       setFocusOn(parseInt(e.target.dataset?.index, 10));
     }
   }, []);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // using this instead of handelling backspace in on handlekeydown
+    // android does not send correct keycode for backspace
+    // and this will work for both delete and backspace too
+    if (e.target.value === "") {
+      value[focusOn] = "";
+      setFocusOn(Math.max(focusOn - 1, 0));
+      setValue([...value]);
+    }
+  };
   return (
     <div
-      onInput={(e) => console.log(e)}
       onPaste={handlePaste}
       onKeyDown={handleKeyDown}
       onClick={moveFocusOnClick}
@@ -74,9 +82,9 @@ export function OtpInput({ length = 6 }: OtpInputPropsType) {
           key={i}
           index={i}
           focus={focusOn === i}
+          onChange={onChange}
         />
       ))}
-      <input type="text" />
     </div>
   );
 }
@@ -85,11 +93,22 @@ function SingleDigitInput({
   val = "",
   focus = false,
   index,
+  onChange,
 }: SingleDigitInputPropsType) {
   const ref = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (focus) {
-      ref.current?.focus();
+  useLayoutEffect(() => {
+    if (ref.current && focus) {
+      ref.current.focus();
+      // need this to make sure the cursor always stays at the end of text
+      // so its easy for user to delete content using backspace
+      const val = ref.current.value;
+      ref.current.value = "";
+      requestAnimationFrame(() => {
+        // Typescript makes it mandatory to have following if check
+        if (ref.current) {
+          ref.current.value = val;
+        }
+      });
     }
   }, [focus]);
   return (
@@ -97,9 +116,12 @@ function SingleDigitInput({
       ref={ref}
       inputMode="numeric"
       value={val}
+      // need this to make sure that its a controlled input
+      onChange={onChange}
       required
       type="text"
       // need this to remove the keyboard arrows that ios keyboard shows
+      // need to keep it zero for first input box so its keyboard focusable
       tabIndex={index !== 0 ? -1 : 0}
       maxLength={1}
       data-index={index}
